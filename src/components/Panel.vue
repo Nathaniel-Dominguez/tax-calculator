6d03223e72353cf7520f0378f13e9dc77d27bdc2
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-05-29 11:53:12 -0700

    added comments

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="panel">
 		<div class="header">
+			<!-- For one way data binding in vuejs, we use textinterpolation. That's what the headline keyword in the brackets below does as a data object.-->
 			<h2 class="headline">{{ headline }}</h2>
 		</div>
 		<div class="body">
+			<!-- Using vue's slot element, which allows us to render child elements of our panel component within the body element. -->
 			<slot />
 		</div>
 	</div>
